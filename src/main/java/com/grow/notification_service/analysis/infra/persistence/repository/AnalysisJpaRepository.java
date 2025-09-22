cb188706bb1a78f380361216e4722237e9a7437d
wkdan <jmy8876@gmail.com>
2025-09-23 00:05:45 +0900

    ✨ feat: 카테고리 id, 세션 id로 Analysis 객체를 찾는 메서드 추가

--- src/main/java/com/grow/notification_service/analysis/infra/persistence/repository/AnalysisJpaRepository.java
+++ src/main/java/com/grow/notification_service/analysis/infra/persistence/repository/AnalysisJpaRepository.java
@@ -1,8 +1,11 @@
 package com.grow.notification_service.analysis.infra.persistence.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.grow.notification_service.analysis.infra.persistence.entity.AnalysisJpaEntity;
 
 public interface AnalysisJpaRepository extends JpaRepository<AnalysisJpaEntity,Long> {
+	Optional<AnalysisJpaEntity> findTopByCategoryIdAndSessionIdOrderByAnalysisIdDesc(Long categoryId, String sessionId);
 }
