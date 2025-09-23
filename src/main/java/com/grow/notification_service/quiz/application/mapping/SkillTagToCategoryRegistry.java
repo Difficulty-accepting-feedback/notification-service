4a5d3b1dd509f49c0993eb4613bccad649ce971f
wkdan <jmy8876@gmail.com>
2025-09-23 23:50:32 +0900

    ✨ feat: 카테고리 확장

--- src/main/java/com/grow/notification_service/quiz/application/mapping/SkillTagToCategoryRegistry.java
+++ src/main/java/com/grow/notification_service/quiz/application/mapping/SkillTagToCategoryRegistry.java
@@ -16,10 +16,57 @@ public class SkillTagToCategoryRegistry {
 
 	// 매핑: SkillTag.name() -> quiz.categoryId
 	private static final Map<String, Long> MAP = Map.ofEntries(
+		// STUDY
 		Map.entry("JAVA_PROGRAMMING", 1L),
-		Map.entry("ENGLISH_CONVERSATION", 2L),
-		Map.entry("PYTHON_DATA_SCIENCE", 3L)
-		// 필요시 추가
+		Map.entry("PYTHON_DATA_SCIENCE", 2L),
+		Map.entry("ENGLISH_CONVERSATION", 3L),
+		Map.entry("MATH_PROBLEM_SOLVING", 4L),
+		Map.entry("HISTORY_EXPLORATION", 5L),
+		Map.entry("FRENCH_LANGUAGE", 6L),
+		Map.entry("ECONOMICS", 7L),
+		Map.entry("AI_MACHINE_LEARNING", 8L),
+		Map.entry("WEB_DEVELOPMENT", 9L),
+		Map.entry("DATABASE_MANAGEMENT", 10L),
+		Map.entry("TOEIC", 11L),
+
+		// HOBBY
+		Map.entry("HIKING", 12L),
+		Map.entry("COOKING", 13L),
+		Map.entry("GUITAR_PLAYING", 14L),
+		Map.entry("PHOTOGRAPHY", 15L),
+		Map.entry("BOOK_READING", 16L),
+		Map.entry("GARDENING", 17L),
+		Map.entry("BOARD_GAMES", 18L),
+		Map.entry("PAINTING", 19L),
+		Map.entry("YOGA", 20L),
+		Map.entry("DANCING", 21L),
+		Map.entry("FISHING", 22L),
+		Map.entry("CYCLING", 23L),
+		Map.entry("KNITTING", 24L),
+		Map.entry("TRAVEL_PLANNING", 25L),
+		Map.entry("MOVIE_APPRECIATION", 26L),
+
+		// MENTORING
+		Map.entry("CAREER_COACHING", 27L),
+		Map.entry("STARTUP_GUIDANCE", 28L),
+		Map.entry("LEADERSHIP_COACHING", 29L),
+		Map.entry("IT_JOB_PREPARATION", 30L),
+		Map.entry("ART_MENTORING", 31L),
+		Map.entry("FINANCIAL_INVESTMENT", 32L),
+		Map.entry("HEALTH_COACHING", 33L),
+		Map.entry("PUBLIC_SPEAKING", 34L),
+		Map.entry("PROJECT_MANAGEMENT", 35L),
+		Map.entry("MARKETING_STRATEGY", 36L),
+		Map.entry("NEGOTIATION_SKILLS", 37L),
+		Map.entry("TIME_MANAGEMENT", 38L),
+		Map.entry("CREATIVE_WRITING", 39L),
+		Map.entry("NETWORKING", 40L),
+		Map.entry("EMOTIONAL_INTELLIGENCE", 41L),
+		Map.entry("BACKEND_DEVELOPMENT", 42L),
+		Map.entry("FRONTEND_DEVELOPMENT", 43L),
+		Map.entry("MOBILE_APP_DEVELOPMENT", 44L),
+		Map.entry("GRAPHIC_DESIGN", 45L),
+		Map.entry("CONTENT_CREATION", 46L)
 	);
 
 	// 매핑: quiz.categoryId -> SkillTag.name()
